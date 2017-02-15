b52aac9ddbe99e78738b2a684bb8b7e81f138066
voongd <david.voong@productmadness.com>
2017-02-15 09:08:17 +0000

    added tests for balance initialisation

--- voong_finance_app/static/js/home.js
+++ voong_finance_app/static/js/home.js
@@ -17,3 +17,14 @@ vf.home = {
 }
 
 $.ready(vf.home.initialise);
+
+var balance_initialisation = new function(){
+    this.div = $('#balance-initialisation');
+    this.input = $('#input');
+    this.submit_button = this.div.find('#submit-button');
+    this.submit_button.click(function(){
+	console.log('submit_button clicked');
+    });
+}
+
+console.log(balance_initialisation);
