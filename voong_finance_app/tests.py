c3b97785de0bc9965e8cfe01a0345d38bbfdcb9f
voongd <david.voong@productmadness.com>
2017-02-21 09:06:24 +0000

    Add tests for balance-initialisation server response

--- voong_finance_app/tests.py
+++ voong_finance_app/tests.py
@@ -1,9 +1,10 @@
+import json
 from django.test import TestCase
 from django.urls import resolve
 from .views import initialise_balance
 
 # Create your tests here.
-class MyTest(TestCase):
+class TestInitialiseBalance(TestCase):
 
     def setUp(self):
         pass
@@ -11,7 +12,13 @@ class MyTest(TestCase):
     def tearDown(self):
         pass
 
-    def test(self):
+    def test_url_resolution(self):
 
         resolver = resolve('/api/initialise-balance')
         self.assertEqual(resolver.func, initialise_balance)
+
+    def test_returns_the_balance_in_json_object(self):
+
+        response = self.client.post('/api/initialise-balance', {'balance': 4344.40})
+        expected = {'date': 'TODO', 'balance': 4344.40}
+        self.assertEqual(expected, response.json(), 'TODO')
--- voong_finance_app/views.py
+++ voong_finance_app/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
     return render(request, 'voong_finance_app/welcome.html')
 
 def initialise_balance(request):
-    pass
+    return JsonResponse({'date': None, 'balance': float(request.POST['balance'])})
